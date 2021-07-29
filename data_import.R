2481d196f352bac9f81e458ffd4be8110d6501d5
Matthew E. Clapham <mclapham@ucsc.edu>
2021-07-28 17:52:57 -0700

    Update data_import.R
    Add show=class to get taxonomy.

--- data_import.R
+++ data_import.R
@@ -7,9 +7,9 @@ source("https://raw.githubusercontent.com/mclapham/PBDB-R-scripts/master/single_
 #Cretaceous bony fishes from marine environments
 #excludes records where genus name is uncertain
 #bony fishes
-fish_data_raw <- read.csv("https://paleobiodb.org/data1.2/occs/list.txt?base_name=Actinopterygii&envtype=marine&interval=Cretaceous&idreso=genus&idqual=genus_certain")
+fish_data_raw <- read.csv("https://paleobiodb.org/data1.2/occs/list.txt?base_name=Actinopterygii&envtype=marine&interval=Cretaceous&idreso=genus&idqual=genus_certain&show=class")
 #all invertebrates (this will be a big file, so will take a while)
-invert_data_raw <- read.csv("https://paleobiodb.org/data1.2/occs/list.txt?base_name=Mollusca,Brachiopoda,Cnidaria,Porifera,Echinodermata,Bryozoa,Ostracoda,Malacostraca&envtype=marine&interval=Cretaceous&idreso=genus&idqual=genus_certain")
+invert_data_raw <- read.csv("https://paleobiodb.org/data1.2/occs/list.txt?base_name=Mollusca,Brachiopoda,Cnidaria,Porifera,Echinodermata,Bryozoa,Ostracoda,Malacostraca&envtype=marine&interval=Cretaceous&idreso=genus&idqual=genus_certain&show=class")
 
 #cleans raw data file
 fish_data_cleaned <- single.stage(fish_data_raw)
