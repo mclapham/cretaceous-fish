683294343bf5ba85ab7b0d97cd1fd6d1a167af7c
ethayu <ethayu1@GMAIL.COM>
2021-07-31 12:47:03 -0700

    Update data_import.R
    add preprocessing to account for lazarus taxa at nBt, nBl, and nB calculation

--- data_import.R
+++ data_import.R
@@ -20,6 +20,37 @@ fish_data_cleaned <- single.stage(fish_data_raw)
 #load in time csv
 interval_order <- read.csv("https://paleobiodb.org/data1.2/intervals/list.txt?scale=1&max_ma=145&min_ma=66&scale_level=5")
 
-#sort by interval
+#iterate through dataframe
 fish_data_age <- left_join(fish_data_cleaned, interval_order, by = c("max_stage" = "interval_name"))
+
 fish_data_sorted <- arrange(fish_data_age, desc(interval_no))
+
+#preprocess data to account for lazarus taxa
+column_names <- unique(fish_data_sorted$genus)
+occurrence_intervals <- array(c(1), dim = c(2, length(column_names)), dimnames = list(c(1, 2),column_names))
+for (row in 1:nrow(fish_data_sorted)) {
+  current_genus <- fish_data_sorted[row, 'genus']
+  if (occurrence_intervals[1, current_genus] == 1) {
+    occurrence_intervals[1, current_genus] <- fish_data_sorted[row, 'interval_no']
+    occurrence_intervals[2, current_genus] <- fish_data_sorted[row, 'interval_no'] #to eliminate empty end interval values in the case of single occurrences
+  } else occurrence_intervals[2, current_genus] <- fish_data_sorted[row, 'interval_no']
+}
+
+#calculating nBt, nBl, nB for each set of intervals using interval_orders
+for(x in c(122, 121, 120, 119, 118, 117, 116, 115, 114, 113)) {
+  occurrences_before <- unique(fish_data_sorted[fish_data_sorted$interval_no == x + 1,]$genus)
+  occurrences_current <- unique(fish_data_sorted[fish_data_sorted$interval_no == x,]$genus)
+  occurrences_after <- unique(fish_data_sorted[fish_data_sorted$interval_no == x - 1,]$genus)
+  for(col in 1:ncol(occurrence_intervals)) {
+    if (x + 1 >= occurrence_intervals[1, col] & x + 1 <= occurrence_intervals[2, col]) append(occurrences_before, col, after = 1)
+    if (x >= occurrence_intervals[1, col] & x <= occurrence_intervals[2, col]) append(occurrences_current, col, after = 1)
+    if (x - 1 >= occurrence_intervals[1, col] & x - 1 <= occurrence_intervals[2, col]) append(occurrences_after, col, after = 1)
+  }
+  occurrences_before <- unique(occurrences_before)
+  occurrences_current <- unique(occurrences_current)
+  occurrences_after <- unique(occurrences_after)
+  
+  nBt <- length(unique(intersect(occurrences_before, occurrences_after)))
+  nBl <- length(unique(intersect(occurrences_before, occurrences_current)))
+  nB <- nBt + nBl
+}
